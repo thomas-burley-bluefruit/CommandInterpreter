c8e3aa2be66cd365edf8f5418cc54a78d4147fce
Tim hvaal <timothy.hvaal@bluefruit.co.uk>
2023-07-25 19:10:58 +0100

    run matched registered command

--- Source/Terminal/Terminal.cpp
+++ Source/Terminal/Terminal.cpp
@@ -7,8 +7,33 @@ Terminal::Terminal(UartInterface& uart) : mUart(uart)
 
 void Terminal::OnReceiveInterrupt(const uint8_t byte)
 {
+    // fill buffer
     char character = static_cast<char>(byte);
-    mCommand.GetRawCommand()[mCommandIndex++] = character;
+    mCommand.GetRawCommand()[mCommandIndex] = character;
+    if (character != '\n')
+    {
+        mCommandIndex++;
+        return;
+    }
+
+    // set command name
+    mCommand.GetRawCommand()[mCommandIndex] = '\0';
+    CommandNames extractedName = CommandNames::none;
+    if (!StringToCommandName(mCommand.GetRawCommand().data(), extractedName))
+    {
+        return; // TODO
+    }
+    mCommand.SetName(extractedName);
+
+    // run command
+    for (CommandHandlerInterface* handler : mCommandHandlers)
+    {
+        if(handler->Name() == mCommand.GetName())
+        {
+            handler->Run(mCommand);
+            return;
+        }
+    }
 }
 
 void Terminal::RegisterCommandHandler(CommandHandlerInterface* handler)
